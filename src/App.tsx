2e25e6c3e91d0a009227884794b035b623b64896
Heorhii Pluhovyi <heorhiipluhovui@gmail.com>
2023-02-10 10:55:23 +0200

    add gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function computeInitialCount() {
+  const initialCount = Math.round(Math.random() * 100);
+  console.log(`initian couin is ${initialCount}`);
+  
+  return initialCount;
+}
+
 function App() {
+  // const defCount: number = 0
+  // const [ count, setCount ] = useState(defCount);
+
+  const defCount: number = 0
+  const [ count, setCount ] = useState(() => computeInitialCount());
+
+  function increment() {
+    console.log('increase by 1')
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    console.log('decrease by 1')
+    setCount(count - 1);
+  }
+
+  function reset() {
+    console.log('reset to zero')
+    setCount(defCount);
+  }
+
+  function async() {
+    setCount((prev: number): number => {
+      console.log('async increace by 1');
+      return prev + 1;
+    });
+
+    setCount((prev: number): number => {
+      console.log('async increace by 1');
+      return prev + 1;
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__counter">
+        <div className="App__counter--count">
+          { count }
+        </div>
+
+        <div className="App__counter--buttons-blok">
+          <button className='App_couner--button button-primary' onClick = { increment }>Add</button>
+          <button className='App_couner--button button-danger' onClick = { decrement }>Sub</button>
+          <button className='App_couner--button button-reset' onClick = { reset }>Res</button>
+          <button className='App_couner--button button-async' onClick = { async }>Async</button>
+        </div>
+      </div>
     </div>
   );
 }
